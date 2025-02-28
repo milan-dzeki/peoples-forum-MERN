996a7ef62ccb7f17696c6e9c4b1faef246c2c405
Milan Pavicevic <lanmi992@live.com>
2025-02-28 12:52:58 +0100

    BACKEND - add module-aliases in server.ts to be able to have proper imports in dist files

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("module-alias/register");
 const http_1 = __importDefault(require("http"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
