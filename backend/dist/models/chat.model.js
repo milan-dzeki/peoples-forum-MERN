740d983044287433cc52fcdec10b3a312385b101
Milan Pavicevic <lanmi992@live.com>
2025-02-28 10:43:05 +0100

    BACKEND - add chat comment schema / model

---
+++ backend/dist/models/chat.model.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const chatSchema = new mongoose_1.Schema({
+    lastMessage: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'Message'
+    },
+    // if it exists it means it is group or community chat
+    chatImageUrl: {
+        type: String,
+        default: null
+    },
+    chatImagePublicId: {
+        type: String,
+        default: null,
+        select: false
+    },
+    members: [
+        {
+            type: mongoose_1.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    // if it has length it means that it is group chat
+    admins: [
+        {
+            type: mongoose_1.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    // if exists it means this is community chat
+    communityId: {
+        type: mongoose_1.Schema.Types.ObjectId,
+        ref: 'Community',
+        default: null
+    },
+    // banned users cannot see chat (used mainly for community chats)
+    bannedUsers: [
+        {
+            type: mongoose_1.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+}, {
+    timestamps: true
+});
+const Chat = mongoose_1.models.Chat || (0, mongoose_1.model)('Chat', chatSchema);
+exports.default = Chat;
---
+++ backend/src/models/chat.model.ts
@@ -0,0 +1,50 @@
+import { Schema, models, model } from 'mongoose';
+
+const chatSchema = new Schema({
+  lastMessage: {
+    type: Schema.Types.ObjectId,
+    ref: 'Message'
+  },
+  // if it exists it means it is group or community chat
+  chatImageUrl: {
+    type: String,
+    default: null
+  },
+  chatImagePublicId: {
+    type: String,
+    default: null,
+    select: false
+  },
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  // if it has length it means that it is group chat
+  admins: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  // if exists it means this is community chat
+  communityId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Community',
+    default: null
+  },
+  // banned users cannot see chat (used mainly for community chats)
+  bannedUsers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ]
+}, {
+  timestamps: true
+});
+
+const Chat = models.Chat || model('Chat', chatSchema);
+
+export default Chat;
