edcef19f5fa8cbc1be0d968352fa7718aa16bee6
Milan Pavicevic <lanmi992@live.com>
2025-02-28 23:43:44 +0100

    BACKEND - add cookieParser middleware in app.ts

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,10 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const error_contoller_1 = __importDefault(require("controllers/error.contoller"));
 const auth_router_1 = __importDefault(require("routes/auth.router"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
 app.use('/api/v0/auth', auth_router_1.default);
 app.use(error_contoller_1.default);
 exports.default = app;
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
 import globalErrorHandler from 'controllers/error.contoller';
 import authRoutes from 'routes/auth.router';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/v0/auth', authRoutes);
 
 app.use(globalErrorHandler);
